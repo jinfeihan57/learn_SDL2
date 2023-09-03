f062bfc014d886ac56971afe7a911657c8bd4b32
jinfeihan57 <jinfeihan57@gmail.com>
2023-09-04 00:13:24 +0800

    hello sdl2

---
+++ hello_SDL2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <SDL.h>
+#include <chrono>
+#include <thread>
+
+constexpr int gWINDOW_WEIGHT = 1152;
+constexpr int gWINDOW_HEIGHT = 896;
+
+int main(int argc, char *argv[])
+{
+    // 初始化SDL
+    SDL_Init(SDL_INIT_VIDEO);
+    // 创建一个 SDL 窗口
+    SDL_Window *screen = SDL_CreateWindow("Hello SDL",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, gWINDOW_WEIGHT, gWINDOW_HEIGHT, 0);
+
+    // 睡眠 2s ，来保持窗口
+    std::chrono::milliseconds ms{2000};
+    std::this_thread::sleep_for(ms);
+
+    // 销毁 SDL 窗口
+    SDL_DestroyWindow(screen);
+    // SDL 退出
+    SDL_Quit();
+
+	return 0;
+}
