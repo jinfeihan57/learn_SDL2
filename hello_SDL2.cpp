a65e963844fdd74312df83806d1bed1fd0b18b23
jinfeihan57 <jinfeihan57@gmail.com>
2023-09-04 22:31:31 +0800

    display a BMP img

--- hello_SDL2.cpp
+++ hello_SDL2.cpp
@@ -15,13 +15,26 @@ int main(int argc, char *argv[])
     // 创建一个 SDL 窗口
     SDL_Window *screen = SDL_CreateWindow("Hello SDL",
         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, gWINDOW_WEIGHT, gWINDOW_HEIGHT, 0);
-
+    // 创建 render
+    SDL_Renderer *render = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED);
+    
+    // 加载一张图片
+    SDL_Surface *surface = SDL_LoadBMP("./hello_SDL2.bmp");
+    if (surface == nullptr) {
+        std::cout << __PRETTY_FUNCTION__ << ": " << __LINE__ << std::endl;
+        return -1;
+    }
+    // 由 surface 转换成 texture
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(render, surface);
+    // 释放 surface
+    SDL_FreeSurface(surface);
     // 睡眠 2s ，来保持窗口
     // std::chrono::milliseconds ms{2000};
     // std::this_thread::sleep_for(ms);
     bool quit = false;
     SDL_Event event;
     while (!quit) {
+        /* deal with event */
         SDL_PollEvent(&event);
         switch (event.type) {
             case SDL_QUIT: {
@@ -33,9 +46,15 @@ int main(int argc, char *argv[])
             default:
                 break;
         }
-        
+        /* do your job */
+        SDL_RenderCopy(render, texture, nullptr, nullptr);
+        SDL_RenderPresent(render);
     }
 
+    // 销毁 texture
+    SDL_DestroyTexture(texture);
+    // 销毁 render
+    SDL_DestroyRenderer(render);
     // 销毁 SDL 窗口
     SDL_DestroyWindow(screen);
     // SDL 退出
