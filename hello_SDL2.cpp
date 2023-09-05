3133af654febe0c95e2a810486fed75c8821ae6c
jinfeihan57 <jinfeihan57@gmail.com>
2023-09-05 20:31:15 +0800

    渲染copy with rectangle

--- hello_SDL2.cpp
+++ hello_SDL2.cpp
@@ -3,8 +3,6 @@
 #include <chrono>
 #include <thread>
 
-// using namespace std::literals::chrono_literals; std::this_thread::sleep_for(2000ms);
-
 constexpr int gWINDOW_WEIGHT = 1152;
 constexpr int gWINDOW_HEIGHT = 896;
 
@@ -17,20 +15,22 @@ int main(int argc, char *argv[])
         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, gWINDOW_WEIGHT, gWINDOW_HEIGHT, 0);
     // 创建 render
     SDL_Renderer *render = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED);
-    
+    int renderW = 0;
+    int renderH = 0;
+    SDL_GetRendererOutputSize(render, &renderW, &renderH);
     // 加载一张图片
     SDL_Surface *surface = SDL_LoadBMP("./hello_SDL2.bmp");
     if (surface == nullptr) {
         std::cout << __PRETTY_FUNCTION__ << ": " << __LINE__ << std::endl;
         return -1;
     }
+    int imgW = surface->w;
+    int imgH = surface->h;
     // 由 surface 转换成 texture
     SDL_Texture *texture = SDL_CreateTextureFromSurface(render, surface);
     // 释放 surface
     SDL_FreeSurface(surface);
-    // 睡眠 2s ，来保持窗口
-    // std::chrono::milliseconds ms{2000};
-    // std::this_thread::sleep_for(ms);
+
     bool quit = false;
     SDL_Event event;
     while (!quit) {
@@ -38,7 +38,6 @@ int main(int argc, char *argv[])
         SDL_PollEvent(&event);
         switch (event.type) {
             case SDL_QUIT: {
-                    // std::cout << __LINE__ << std::endl;
                     quit = true;
                     break;
                 }
@@ -47,10 +46,13 @@ int main(int argc, char *argv[])
                 break;
         }
         /* do your job */
-        SDL_Rect sRect{0, 0, 500, 500};
-        SDL_Rect s1Rect{500, 0, 500, 500};
-        SDL_RenderCopy(render, texture, &sRect, &sRect);
-        SDL_RenderCopy(render, texture, &sRect, &s1Rect);
+        SDL_Rect sRect{0, 0, imgW, imgH};
+        SDL_Rect dRect{0, 0, renderW, renderH};
+        SDL_RenderCopy(render, texture, &sRect, &dRect);
+
+        // 显示等比例缩放图
+        // SDL_Rect d1Rect{300, 200, 500, 500};
+        // SDL_RenderCopy(render, texture, &sRect, &d1Rect);
 
         SDL_RenderPresent(render);
     }
