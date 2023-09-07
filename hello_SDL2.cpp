bcb593e15469800816be9b4c987e4a98ef58b206
jinfeihan57 <jinfeihan57@gmail.com>
2023-09-07 23:01:35 +0800

    event hold window

--- hello_SDL2.cpp
+++ hello_SDL2.cpp
@@ -3,8 +3,6 @@
 #include <chrono>
 #include <thread>
 
-// using namespace std::literals::chrono_literals; std::this_thread::sleep_for(2000ms);
-
 constexpr int gWINDOW_WEIGHT = 1152;
 constexpr int gWINDOW_HEIGHT = 896;
 
@@ -17,9 +15,23 @@ int main(int argc, char *argv[])
         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, gWINDOW_WEIGHT, gWINDOW_HEIGHT, 0);
 
     // 睡眠 2s ，来保持窗口
-    std::chrono::milliseconds ms{2000};
-    std::this_thread::sleep_for(ms);
+    // std::chrono::milliseconds ms{2000};
+    // std::this_thread::sleep_for(ms);
 
+    bool quit = false;
+    SDL_Event event;
+    while (!quit) {
+        SDL_PollEvent(&event);
+        switch (event.type) {
+            case SDL_QUIT : {
+                quit = true;
+                break;
+            }
+
+            default:
+                break;
+        }
+    }
     // 销毁 SDL 窗口
     SDL_DestroyWindow(screen);
     // SDL 退出
