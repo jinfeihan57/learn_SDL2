577a60809392001aa7e309ddf4171e085970c955
jinfeihan57 <jinfeihan57@gmail.com>
2023-09-18 22:40:50 +0800

    add fps ctrl

--- hello_SDL2.cpp
+++ hello_SDL2.cpp
@@ -6,15 +6,18 @@
 constexpr int gWINDOW_WEIGHT = 1152;
 constexpr int gWINDOW_HEIGHT = 896;
 
+constexpr int gFPS = 60;
+constexpr Uint32 gFPS_TIME = 1000 / gFPS;
+
 int main(int argc, char *argv[])
 {
     // 初始化SDL
-    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_VIDEO); // SDL_INIT_TIMER?
     // 创建一个 SDL 窗口
     SDL_Window *screen = SDL_CreateWindow("Hello SDL",
         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, gWINDOW_WEIGHT, gWINDOW_HEIGHT, 0);
     // 创建 render
-    SDL_Renderer *render = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Renderer *render = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED);  // SDL_RENDERER_PRESENTVSYNC
     int renderW = 0;
     int renderH = 0;
     SDL_GetRendererOutputSize(render, &renderW, &renderH);
@@ -37,6 +40,8 @@ int main(int argc, char *argv[])
     bool quit = false;
     SDL_Event event;
     while (!quit) {
+        std::chrono::time_point start = std::chrono::high_resolution_clock::now();
+        // Uint32 startMs = SDL_GetTicks();
         /* deal with event */
         SDL_PollEvent(&event);
         switch (event.type) {
@@ -59,7 +64,6 @@ int main(int argc, char *argv[])
             }
             case SDL_MOUSEBUTTONDOWN: {
                 if ((event.button.button == SDL_BUTTON_LEFT) && (event.button.clicks == 2)) { // 双击
-                    std::cout << __LINE__ << std::endl;
                     tigerHeadx = event.motion.x;
                     tigerHeady = event.motion.y;
                 }
@@ -72,13 +76,20 @@ int main(int argc, char *argv[])
         // 清屏
         SDL_SetRenderDrawColor(render, 135, 206, 235, 0xFF );
         SDL_RenderClear(render);
-        // 绘制
-        SDL_SetRenderDrawColor(render, 255, 0, 0, 0xFF );
-        SDL_RenderDrawLine(render, 0, 0, 500, 400);
-        // SDL_RenderSetScale(render, 1, 2);
+
+        SDL_Rect d1Rect{tigerHeadx, tigerHeady, 400, 500};
+        SDL_Rect s1Rect{300, 50, 400, 500};
+        SDL_RenderCopy(render, texture, &s1Rect, &d1Rect);
 
         // 显示
         SDL_RenderPresent(render);
+        
+        // Uint32 endMs = SDL_GetTicks();
+        // Uint32 consumeTime = endMs - startMs;
+        // SDL_Delay(consumeTime >= gFPS_TIME ? 0 : (gFPS_TIME - consumeTime)); // 调整帧率
+
+        std::chrono::time_point end = std::chrono::high_resolution_clock::now();
+        std::cout << "FPS:" << std::chrono::seconds(1) / (end - start) << std::endl;
     }
 
     // 销毁 texture
