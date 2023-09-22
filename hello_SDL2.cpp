10a82e94022c2f7d05ab6ebd272b3ca945caff0c
jinfeihan57 <jinfeihan57@gmail.com>
2023-09-22 22:20:34 +0800

    SDL_IMG

--- hello_SDL2.cpp
+++ hello_SDL2.cpp
@@ -13,12 +13,30 @@ constexpr Uint32 gFPS_TIME = 1000 / gFPS;
 
 int main(int argc, char *argv[])
 {
+    int ret = 0;
+    if ( ! argv[1] ) {
+        std::cout << "Usage: ./main.exe (*.png | *.jpg)" << std::endl;
+        return -1;
+    }
     // 初始化SDL
-    SDL_Init(SDL_INIT_VIDEO); // SDL_INIT_TIMER?
+    ret = SDL_Init(SDL_INIT_VIDEO);
+    if (ret != 0) {
+        std::cout << __PRETTY_FUNCTION__ << ": " << __LINE__ << std::endl;
+        return -1;
+    }
+    // 初始化IMG
+    ret = IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);
+    if (ret != (IMG_INIT_PNG | IMG_INIT_JPG)) {
+        std::cout << __PRETTY_FUNCTION__ << ": " << __LINE__ << std::endl;
+        return -1;
+    }
     // 创建一个 SDL 窗口
     SDL_Window *screen = SDL_CreateWindow("Hello SDL",
         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, gWINDOW_WEIGHT, gWINDOW_HEIGHT, 0);
-    
+    if (screen == nullptr) {
+        std::cout << __PRETTY_FUNCTION__ << ": " << __LINE__ << std::endl;
+        return -1;
+    }
     // 加载一张图片
     SDL_Surface *surfaceIcon = SDL_LoadBMP("./hello_SDL2.bmp");
     if (surfaceIcon == nullptr) {
@@ -30,24 +48,31 @@ int main(int argc, char *argv[])
     // 释放 surfaceIcon
     SDL_FreeSurface(surfaceIcon);
     // 创建 render
-    SDL_Renderer *render = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED);  // SDL_RENDERER_PRESENTVSYNC
-    int renderW = 0;
-    int renderH = 0;
-    SDL_GetRendererOutputSize(render, &renderW, &renderH);
-    // 加载一张图片
-    SDL_Surface *surface = SDL_LoadBMP("./hello_SDL2.bmp");
+    SDL_Renderer *render = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED);
+    if (render == nullptr) {
+        std::cout << __PRETTY_FUNCTION__ << ": " << __LINE__ << std::endl;
+        return -1;
+    }
+
+    // 加载 texture
+    int imgW = 0;
+    int imgH = 0;
+    SDL_Surface *surface = IMG_Load(argv[1]);
     if (surface == nullptr) {
         std::cout << __PRETTY_FUNCTION__ << ": " << __LINE__ << std::endl;
         return -1;
     }
-    int imgW = surface->w;
-    int imgH = surface->h;
+    imgW = surface->w;
+    imgH = surface->h;
     // 由 surface 转换成 texture
     SDL_Texture *texture = SDL_CreateTextureFromSurface(render, surface);
+    if (texture == nullptr) {
+        std::cout << __PRETTY_FUNCTION__ << ": " << __LINE__ << std::endl;
+        return -1;
+    }
     // 释放 surface
     SDL_FreeSurface(surface);
 
-    Uint8 alph = 127;
     int tigerHeadx = 400;
     int tigerHeady = 200;
     bool quit = false;
@@ -87,34 +112,11 @@ int main(int argc, char *argv[])
         }
         /* do your job */
         // 清屏
-        SDL_SetRenderDrawColor(render, 135, 206, 235, 0xFF );
+        SDL_SetRenderDrawColor(render, 135, 206, 0, 0xFF );
         SDL_RenderClear(render);
         // 绘制
-
-        SDL_SetRenderDrawColor(render, 255, 0, 0, 0xFF);
-        SDL_RenderDrawLine(render, 0, 0, 500, 500);
-
-        // SDL_Point points[] = {{500, 500}, {200, 300}, {300, 300}, {400, 200}, {500, 500}};
-        // SDL_RenderDrawLines(render, points, sizeof(points) / sizeof(SDL_Point));
-
-        // SDL_SetRenderDrawColor(render, 0, 0, 255, 0xFF);
-        // SDL_Rect aRect{200, 200, 50, 70};
-        // SDL_RenderDrawRect(render, &aRect);
-
-        // SDL_SetRenderDrawColor(render, 0, 0, 255, 0xFF);
-        // SDL_Rect rects[] = {{400, 300, 100, 200}, {401, 301, 98, 198}, {402, 302, 96, 196},
-        //                     {403, 303, 94, 194}, {404, 304, 92, 192}};
-        // SDL_RenderDrawRects(render, rects, sizeof(rects) / sizeof(SDL_Rect));
-
-        // SDL_SetRenderDrawColor(render, 127, 0, 255, 0xFF);
-        // SDL_Rect bRect{300, 200, 50, 70};
-        // SDL_RenderFillRect(render, &bRect);
-
-        // SDL_SetRenderDrawColor(render, 255, 0, 0, 0xFF);
-        // for(int i = 500; i < 550; i++) {
-        //     SDL_RenderDrawPoint(render, i, i+1);  // SDL_RenderDrawPoints
-        // }
-
+        SDL_Rect dRect = {tigerHeadx, tigerHeady, imgW, imgH};
+        SDL_RenderCopy(render, texture, nullptr, &dRect);
         // 显示
         SDL_RenderPresent(render);
 
@@ -125,7 +127,6 @@ int main(int argc, char *argv[])
 
         std::chrono::time_point end = std::chrono::high_resolution_clock::now();
         int fps = std::chrono::seconds(1) / (end - start);
-        std::cout << "FPS:" << fps << std::endl;
         // 修改应用标题
         SDL_SetWindowTitle(screen, (std::string("fps: ") + std::to_string(fps)).c_str());
     }
@@ -136,6 +137,8 @@ int main(int argc, char *argv[])
     SDL_DestroyRenderer(render);
     // 销毁 SDL 窗口
     SDL_DestroyWindow(screen);
+    // IMG 退出
+    IMG_Quit();
     // SDL 退出
     SDL_Quit();
 
