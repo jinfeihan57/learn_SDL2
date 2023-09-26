d40350f97b166a822d0b2775855eeadbfe8819d4
jinfeihan57 <jinfeihan57@gmail.com>
2023-09-26 20:23:19 +0800

    SDL ttf 英文字符

--- hello_SDL2.cpp
+++ hello_SDL2.cpp
@@ -5,46 +5,48 @@
 #include <vector>
 
 #include "SDL.h"
-#include "SDL_image.h"
+#include "SDL_ttf.h"
 
 constexpr int gWINDOW_WEIGHT = 1152;
 constexpr int gWINDOW_HEIGHT = 896;
 
 constexpr int gFPS = 60;
 constexpr Uint32 gFPS_TIME = 1000 / gFPS;
-
+constexpr int gDEFAULT_PTSIZE = 70;
 int main(int argc, char *argv[])
 {
     int ret = 0;
 
+    if ( ! argv[1] ) {
+        std::cout << "Usage: ./main.exe *.ttf (中文字库)" << std::endl;
+        return -1;
+    }
     // 初始化SDL
     ret = SDL_Init(SDL_INIT_VIDEO);
     if (ret != 0) {
-        std::cout << __PRETTY_FUNCTION__ << ": " << __LINE__ << std::endl;
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s\n",SDL_GetError());
         return -1;
     }
-    // 初始化IMG
-    ret = IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);
-    if (ret != (IMG_INIT_PNG | IMG_INIT_JPG)) {
-        std::cout << __PRETTY_FUNCTION__ << ": " << __LINE__ << std::endl;
+    if (TTF_Init() < 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize TTF: %s\n",SDL_GetError());
         SDL_Quit();
-        return -1;
+        return(2);
     }
     // 创建一个 SDL 窗口
     SDL_Window *screen = SDL_CreateWindow("Hello SDL",
         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, gWINDOW_WEIGHT, gWINDOW_HEIGHT, 0);
     if (screen == nullptr) {
-        std::cout << __PRETTY_FUNCTION__ << ": " << __LINE__ << std::endl;
-        IMG_Quit();
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't Create SDL Window: %s\n",SDL_GetError());
+        TTF_Quit();
         SDL_Quit();
         return -1;
     }
     // 加载一张图片
     SDL_Surface *surfaceIcon = SDL_LoadBMP("./hello_SDL2.bmp");
     if (surfaceIcon == nullptr) {
-        std::cout << __PRETTY_FUNCTION__ << ": " << __LINE__ << std::endl;
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't SDL_LoadBMP: %s\n",SDL_GetError());
         SDL_DestroyWindow(screen);
-        IMG_Quit();
+        TTF_Quit();
         SDL_Quit();
         return -1;
     }
@@ -55,55 +57,38 @@ int main(int argc, char *argv[])
     // 创建 render
     SDL_Renderer *render = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED);
     if (render == nullptr) {
-        std::cout << __PRETTY_FUNCTION__ << ": " << __LINE__ << std::endl;
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't SDL_CreateRenderer: %s\n",SDL_GetError());
         SDL_DestroyWindow(screen);
-        IMG_Quit();
+        TTF_Quit();
         SDL_Quit();
         return -1;
     }
-
-    // 加载 texture
-    SDL_Texture *texture = IMG_LoadTexture(render, "adventurer-sheet.png");
+    int ptsize = gDEFAULT_PTSIZE;
+    TTF_Font *font = TTF_OpenFont(argv[1], ptsize);
+    SDL_Color forecol = {0, 0, 0, 0xff};
+    SDL_Color backcol = {0xff, 0xff, 0xff, 0xff};
+    SDL_Log("Hello SDL! TTF_SetFontSize: %d\n", TTF_SetFontSize(font, 200)); // 在TTF_Render* 字符之前设置才生效，或者每次修改ptsize都重新执行 TTF_Render*
+    SDL_Surface *surface = TTF_RenderText_Blended(font, "Hello SDL!", forecol);
+    // SDL_Surface *surface = TTF_RenderText_Blended_Wrapped(font, "Hello SDL!", forecol, 100);
+    // SDL_Surface *surface = TTF_RenderText_LCD(font, "Hello SDL!", forecol, backcol);
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(render, surface);
     if (texture == nullptr) {
-        std::cout << __PRETTY_FUNCTION__ << ": " << __LINE__ << std::endl;
-        SDL_DestroyRenderer(render);
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't SDL_CreateTextureFromSurface: %s\n",SDL_GetError());
         SDL_DestroyWindow(screen);
-        IMG_Quit();
+        TTF_Quit();
         SDL_Quit();
         return -1;
     }
-    int maxDuration = 150;
-    int widthSpr = 50;
-    int heightSpr = 37;
-    int imgW = widthSpr * 3;
-    int imgH = heightSpr * 3;
-
-    std::vector<std::pair<int, int>> idle1  { {0, 0}, {0, 1}, {0, 2}, {0, 3} };
-    std::vector<std::pair<int, int>> crouch { {0, 4}, {0, 5}, {0, 6}, {1, 0} };
-    std::vector<std::pair<int, int>> run    { {1, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5}, {1, 6} };
-    std::vector<std::pair<int, int>> jump   { {2, 0}, {2, 1}, {2, 2}, {2, 3} };
-    std::vector<std::pair<int, int>> mid    { {2, 4}, {2, 5}, {2, 6}, {3, 0} };
-    std::vector<std::pair<int, int>> fall   { {3, 1}, {3, 2} };
-    std::vector<std::pair<int, int>> slide  { {3, 3}, {3, 4}, {3, 5}, {3, 6}, {4, 0} };
-    std::vector<std::pair<int, int>> grab   { {4, 1}, {4, 2}, {4, 3}, {4, 4}};
-    std::vector<std::pair<int, int>> climb  { {4, 5}, {4, 6}, {5, 0}, {5, 1}, {5, 2} };
-    std::vector<std::pair<int, int>> idle2  { {5, 3}, {5, 4}, {5, 5}, {5, 6} };
-    std::vector<std::pair<int, int>> attack1{ {6, 0}, {6, 1}, {6, 2}, {6, 3}, {6, 4} };
-    std::vector<std::pair<int, int>> attack2{ {6, 5}, {6, 6}, {7, 0}, {7, 1}, {7, 2}, {7, 3} };
-    std::vector<std::pair<int, int>> attack3{ {7, 4}, {7, 5}, {7, 6}, {8, 0}, {8, 1}, {8, 2} };
-    std::vector<std::pair<int, int>> hurt   { {8, 3}, {8, 4}, {8, 5} };
-    std::vector<std::pair<int, int>> die    { {8, 6}, {9, 0}, {9, 1}, {9, 2}, {9, 3}, {9, 4}, {9, 5} };
-    std::vector<std::pair<int, int>> jump2  { {9, 6}, {10, 0}, {10, 1} };
-    int index = 0;
-    std::vector<std::pair<int, int>> current = idle1;
-    int currentTime = SDL_GetTicks();
-    int spriteChangeTime = currentTime + maxDuration;
-    SDL_bool faceRight = SDL_TRUE;
-    SDL_RendererFlip flip = SDL_FLIP_NONE;
-
-    int keyboardEvent = 0;
+    SDL_Log("Hello SDL! surface w: %d\n", surface->w);
+    SDL_Log("Hello SDL! surface h: %d\n", surface->h);
     int tigerHeadx = 400;
     int tigerHeady = 200;
+    int helloWidth = surface->w;
+    int helloHigh = surface->h;
+    SDL_Rect helloRect = {tigerHeadx, tigerHeady, helloWidth, helloHigh};
+    SDL_FreeSurface(surface);
+
+    int keyboardEvent = 0;
     bool quit = false;
     SDL_Event event;
     while (!quit) {
@@ -111,69 +96,29 @@ int main(int argc, char *argv[])
         Uint32 startMs = SDL_GetTicks();
         /* deal with event */
         const Uint8 *keyStatus = SDL_GetKeyboardState(&keyboardEvent);
-        // std::cout << keyboardEvent << std::endl;
-        if (keyStatus[SDL_SCANCODE_Q] == SDL_PRESSED) {
-            index = 0;
-            current = idle1;
-        } else if (keyStatus[SDL_SCANCODE_W] == SDL_PRESSED) {
-            index = 0;
-            current = crouch;
-        } else if (keyStatus[SDL_SCANCODE_E] == SDL_PRESSED) {
-            index = 0;
-            current = run;
-        } else if (keyStatus[SDL_SCANCODE_SPACE] == SDL_PRESSED) {
-            index = 0;
-            current = jump;
-        } else if (keyStatus[SDL_SCANCODE_T] == SDL_PRESSED) {
-            index = 0;
-            current = mid;
-        } else if (keyStatus[SDL_SCANCODE_Y] == SDL_PRESSED) {
-            index = 0;
-            current = fall;
-        } else if (keyStatus[SDL_SCANCODE_U] == SDL_PRESSED) {
-            index = 0;
-            current = slide;
-        } else if (keyStatus[SDL_SCANCODE_I] == SDL_PRESSED) {
-            index = 0;
-            current = grab;
-        } else if (keyStatus[SDL_SCANCODE_O] == SDL_PRESSED) {
-            index = 0;
-            current = climb;
-        } else if (keyStatus[SDL_SCANCODE_P] == SDL_PRESSED) {
-            index = 0;
-            current = idle2;
-        } else if (keyStatus[SDL_SCANCODE_A] == SDL_PRESSED) {
-            index = 0;
-            current = attack1;
-        } else if (keyStatus[SDL_SCANCODE_S] == SDL_PRESSED) {
-            index = 0;
-            current = attack2;
-        } else if (keyStatus[SDL_SCANCODE_D] == SDL_PRESSED) {
-            index = 0;
-            current = attack3;
-        } else if (keyStatus[SDL_SCANCODE_F] == SDL_PRESSED) {
-            index = 0;
-            current = hurt;
-        } else if (keyStatus[SDL_SCANCODE_G] == SDL_PRESSED) {
-            index = 0;
-            current = die;
-        } else if (keyStatus[SDL_SCANCODE_H] == SDL_PRESSED) {
-            index = 0;
-            current = jump2;
-        }
         if (keyStatus[SDL_SCANCODE_RIGHT] == SDL_PRESSED) {
-            tigerHeadx += 1;
-            flip = SDL_FLIP_NONE;
+            tigerHeadx += 3;
         }
         if (keyStatus[SDL_SCANCODE_LEFT] == SDL_PRESSED) {
-            tigerHeadx -= 1;
-            flip = SDL_FLIP_HORIZONTAL;
+            tigerHeadx -= 3;
         }
         if (keyStatus[SDL_SCANCODE_DOWN] == SDL_PRESSED) {
-            tigerHeady += 1;
+            tigerHeady += 3;
         }
         if (keyStatus[SDL_SCANCODE_UP] == SDL_PRESSED) {
-            tigerHeady -= 1;
+            tigerHeady -= 3;
+        }
+        if (keyStatus[SDL_SCANCODE_W] == SDL_PRESSED) {
+            helloHigh += 3;
+        }
+        if (keyStatus[SDL_SCANCODE_S] == SDL_PRESSED) {
+            helloHigh -= 3;
+        }
+        if (keyStatus[SDL_SCANCODE_A] == SDL_PRESSED) {
+            helloWidth -= 3;
+        }
+        if (keyStatus[SDL_SCANCODE_D] == SDL_PRESSED) {
+            helloWidth += 3;
         }
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
@@ -185,19 +130,12 @@ int main(int argc, char *argv[])
         SDL_SetRenderDrawColor(render, 135, 206, 0, 0xFF );
         SDL_RenderClear(render);
         // 绘制
-        SDL_Rect sRect = {current[index].second * widthSpr, current[index].first * heightSpr, widthSpr, heightSpr};
-        SDL_Rect dRect = {tigerHeadx, tigerHeady, imgW, imgH};
-        SDL_RenderCopyEx(render, texture, &sRect, &dRect, 0, nullptr, flip);
+        helloRect.x = tigerHeadx;
+        helloRect.y = tigerHeady;
+        helloRect.w = helloWidth;
+        helloRect.h = helloHigh;
+        SDL_RenderCopy(render, texture, nullptr, &helloRect);
 
-        currentTime = SDL_GetTicks();
-        if (currentTime > spriteChangeTime) {
-            spriteChangeTime += maxDuration;
-            index++;
-            if ((index % current.size()) == 0) {
-                index = 0;
-                current = run;
-            }
-        }
         // 显示
         SDL_RenderPresent(render);
 
@@ -214,12 +152,14 @@ int main(int argc, char *argv[])
 
     // 销毁 texture
     SDL_DestroyTexture(texture);
+    // 关闭 font
+    TTF_CloseFont(font);
     // 销毁 render
     SDL_DestroyRenderer(render);
     // 销毁 SDL 窗口
     SDL_DestroyWindow(screen);
-    // IMG 退出
-    IMG_Quit();
+    // TTF 退出
+    TTF_Quit();
     // SDL 退出
     SDL_Quit();
 
